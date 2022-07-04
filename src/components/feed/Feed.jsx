d3c0fad02a79b73a7ed0f5d1d748f29655352e82
Rohithj47 <rohithjanardhan98@gmail.com>
2022-07-04 22:44:41 +0530

    Home page Components

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './feed.css'
+function Feed(props) {
+    return (
+        <div className='feed'>
+            Hi From Feed
+        </div>
+    );
+}
+export default Feed;
---
+++ src/components/rightbar/Rightbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './rightbar.css'
+
+function Rightbar(props) {
+    return (
+        <div className='rightbar'>
+            Hi From Righbar
+        </div>
+    );
+}
+
+export default Rightbar;
